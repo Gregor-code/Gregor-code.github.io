4a17b4e4bd375f5bc994c5b1cf987028cdc0c85c
Gregor-code <73022580+Gregor-code@users.noreply.github.com>
2021-04-22 19:18:05 +0200

    burger animation created

--- sidebar.js
+++ sidebar.js
@@ -2,9 +2,11 @@ const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".link-holder");
     const navLinks = document.querySelectorAll(".link-holder li");
+    const Line2 = document.querySelector(".line2");
 
     burger.addEventListener("click", () => {
         nav.classList.toggle("nav-active");
+        Line2.classList.toggle("burger-active");
     });
 
     navLinks.forEach((link, index) => {
